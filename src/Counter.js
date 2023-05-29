c0981fa3bfa8e5a74b9e1f46663d9b0d47657610
mthtanjim <mthtanjim@gmail.com>
2023-05-29 17:40:29 +0600

    crating counterSlice

--- src/Counter.js
+++ src/Counter.js
@@ -12,11 +12,32 @@ const Counter = () => {
     const clear = () => {
         setCount(null)
     }
+    const obj = {
+        a: {
+            ac: 5
+        }, 
+        b: 55
+    }
+    const objCopy = {
+        ...obj, 
+        b: 60
+    }
+
+    const arr = ['a', 'b']
+    // Create a new copy of arr, with "c" appended to the end
+    const newArr2 = arr.slice()
+    newArr2.push("m")
+    newArr2.push("n")
+    newArr2.push("o")
+
     return (
         <div>
             
             <h4>Total count is: {count}
             </h4>
+            <pre>{JSON.stringify(obj, null, 8)}</pre>
+            <pre>{JSON.stringify(objCopy, null, 8)}</pre>
+            <pre>{JSON.stringify(newArr2, null, 8)}</pre>
             <button onClick={increment} >
                 Click me
             </button>
