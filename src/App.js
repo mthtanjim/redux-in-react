97e704432bc14a7f1587aa5a0ab48fea365f224e
mthtanjim <mthtanjim@gmail.com>
2023-05-30 17:59:17 +0600

    toolkits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
+
+import { Counter } from './features/Counter';
+import logo from './logo.svg';
 
 import './App.css';
-import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-     
-      <h2>Simple Counter</h2>
-
-      <Counter/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+      </header>
     </div>
   );
 }
